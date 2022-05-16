b91e7cd655ca7802b27aac8b56bc8f033953cf18
The7thHeart <elherzahmedonline@gmail.com>
2022-05-16 15:35:55 +0100

    login request

---
+++ src/components/Login/Auth.js
@@ -0,0 +1,19 @@
+
+
+
+
+
+const Auth = async (email, password) => {
+    const link = 'http://localhost:8080/auth?email='+encodeURIComponent(email)+'&password='+encodeURIComponent(password);
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'text/plain' },
+        //body: JSON.stringify({ email: email, password: password })
+    };
+    const token = await fetch(link,requestOptions).then(res => res.text())
+
+    return token;
+    
+
+}
+export default Auth;
