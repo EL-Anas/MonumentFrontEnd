05777650f8cbdb3b77e4219a082d38157ec58caa
anasel <anas.elalaoui2000@gmail.com>
2022-05-18 11:26:49 +0100

    field validation in admonument

--- src/components/AddMonument/AddMonument.jsx
+++ src/components/AddMonument/AddMonument.jsx
@@ -10,6 +10,8 @@ import {
 } from "@mui/material";
 
 import MenuItem from "@mui/material/MenuItem";
+import {useNavigate} from 'react-router-dom';
+import Alert from '@mui/material/Alert';
 
 export default function AddMonument() {
 
@@ -21,6 +23,23 @@ export default function AddMonument() {
   const[img1,setImg1]=React.useState("");
   const[img2,setImg2]=React.useState("");
   const[img3,setImg3]=React.useState("");
+  const[submit,setSubmit]=React.useState(false);
+  const[returnid,setReturnId]=React.useState("fail");
+  const navigate = useNavigate();
+  const handleEmptyValidation=(field)=>{
+    if(submit)
+      return field!=="";
+    return true;
+}
+const verifyAllFields=()=>{
+  setSubmit(true);
+  return handleEmptyValidation(nom)&&handleEmptyValidation(longitude)&&handleEmptyValidation(latitude)&&handleEmptyValidation(ville)&&handleEmptyValidation(img1);
+}
+const redirect=(field)=>{
+  if(field!=="fail"){
+    setTimeout(() => {  navigate("/monument/"+field); }, 2000);
+  }
+}
   const handleClick =async (e) => {
     e.preventDefault() ;
     const Monument =
@@ -49,7 +68,7 @@ export default function AddMonument() {
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(Monument)
     };
-     fetch(link,requestOptions);
+     if(verifyAllFields()){fetch(link,requestOptions).then(res=>res.text()).then(returnid=>{setReturnId(returnid);redirect(returnid)});}
   }
   return (
     <div className="App">
@@ -59,11 +78,13 @@ export default function AddMonument() {
 
       <form>
         <TextField
+          required
           style={{ width: "500px", margin: "5px" }}
           type="text"
           label="Nom"
           variant="outlined"
           onChange={(event) =>setNom(event.target.value)}
+          error={!handleEmptyValidation(nom)}
         />
         <br />
         <TextField
@@ -75,35 +96,43 @@ export default function AddMonument() {
         />
         <br />
         <TextField
+          required
           style={{ width: "500px", margin: "5px" }}
           type="number"
           label="Longitude"
           variant="outlined"
           onChange={(event) =>setLongitude(event.target.value)}
+          error={!handleEmptyValidation(longitude)}
         />
         <br />
         <TextField
+          required
           style={{ width: "500px", margin: "5px" }}
           type="number"
           label="Latitude"
           variant="outlined"
           onChange={(event) =>setLatitude(event.target.value)}
+          error={!handleEmptyValidation(latitude)}
         />
         <br />
         <TextField
+          required
           style={{ width: "500px", margin: "5px" }}
           type="text"
           label="Ville"
           variant="outlined"
           onChange={(event) =>setVille(event.target.value)}
+          error={!handleEmptyValidation(ville)}
         />
         <br />
         <TextField
+          required
           style={{ width: "500px", margin: "5px" }}
           type="text"
           label="Image Link 1"
           variant="outlined"
           onChange={(event) =>setImg1(event.target.value)}
+          error={!handleEmptyValidation(img1)}
         />
         <br />
         <TextField
@@ -122,6 +151,7 @@ export default function AddMonument() {
           onChange={(event) =>setImg3(event.target.value)}
         />
         <br />
+        {returnid!=="fail"&&<Alert variant="filled" severity="success">Monument bien créé!</Alert>}
         <br />
         <Button variant="contained" color="primary" onClick={handleClick} >
           Add
